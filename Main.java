13bd8747d6cff2e50f363e3141a2750e96926a2c
Александр Павлючков <team1899@gmail.com>
2024-07-28 11:41:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Employee aleksandr = new Employee("Александр", "Директор", "director@gmail.com", "+375290000007", 100500, 47);
+        aleksandr.getEmployeeInfo();
+     }
+}
