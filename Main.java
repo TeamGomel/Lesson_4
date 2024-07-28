6420b90051e26c463187f905f5e3bcba2479fad4
Александр Павлючков <team1899@gmail.com>
2024-07-28 11:43:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,1 @@
-public class Main {
 
-    public static void main(String[] args) {
-        Employee aleksandr = new Employee("Александр", "Директор", "director@gmail.com", "+375290000007", 100500, 47);
-        aleksandr.getEmployeeInfo();
-     }
-}
